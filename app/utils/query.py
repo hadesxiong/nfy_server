b258ff0f07ecc637071bf14491ba7b9e81ecd79d
hadesxiong <fengshixiong890614@gmail.com>
2024-07-31 18:30:36 +0800

    update build_query_exp functions

--- app/utils/query.py
+++ app/utils/query.py
@@ -2,12 +2,25 @@
 from typing import List, Dict, Any
 from tortoise.expressions import Q
 
-def build_query_exp(filters: List[Dict[str,Any]]) -> Any:
+def build_query_exp(filters:Dict[str,Any],logics:Dict[str,Any]=None) -> Any:
 
-    # 理论上通过传入key,value,logic来组成查询函数
+    '''
+    入参定义：
+    fitlers: 通过request模型形成的字典,key:value类型，其中key指代数据库字段，value指代目标筛选值
+    logics: 定义好的针对filters中可能出现的逻辑方法,key:logic，其中key与filters中保持一致,logic代表tortoise的逻辑运算法后缀
+    通过组装filters+logics来形成query表达式
+    '''
     rslt_query = Q()
-    
-    for each_filter in filters:
+    fltr_list = []
+    for key,value in filters.items():
+        # 检查 logics 是否为 None 或者 key 是否不在 logics 中
+        if logics is None or key not in logics:
+            fltr_list.append({'key': key, 'value': value})
+        else:
+            fltr_list.append({'key': key, 'value': value, 'logic': logics[key]})
+
+
+    for each_filter in fltr_list:
         
         if each_filter.get('key') is None or each_filter.get('value') is None:
             continue
